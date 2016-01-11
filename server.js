eb567aa297a2249019c75849de0b4cf3217a135d
Mats Hindefelt <Mats@Matss-MacBook-Pro.local>
2016-01-11 21:01:06 +0100

    Networking example done

--- server.js
+++ server.js
@@ -6,25 +6,33 @@
  */
 
 // modules
-var static = require( 'node-static' ),
-port = 8080,
-http = require( 'http' ),
-io = require('socket.io')(http);
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-// config
-var file = new static.Server( './', {
-                             cache: 3600,
-                             gzip: true
- 
-                             } );
-io.on('connection', function(socket) {
-      console.log('a user connected');
+server.listen(8080);
+
+app.get('/', function (req, res) {
+        res.sendFile(__dirname + '/public/index.html');
+        });
+
+var col = true;
+var users = 0;
+io.sockets.on('connection', function (socket) {
+      users += 1;
+      console.log('user '+users+' connected');
+      socket.emit('username', {username: 'User'+users});
+      socket.emit('changeColor', {color: "#FF0000"});
+      socket.on('disconnect', function() {
+                console.log('user disconnected');
       });
-
-// serve
-http.createServer( function ( request, response ) {
-                  request.addListener( 'end', function () {
-                                      file.serve( request, response );
-                                      } ).resume();
-                  } ).listen( port );
-
+      socket.on('clicked', function(data) {
+                col = !col;
+                //console.log(data.username+' has clicked the canvas');
+                if(col) {
+                    io.emit('changeColor', {color: "#FF0000"});
+                } else {
+                    io.emit('changeColor', {color: "#00FF00"});
+                }
+        });
+});
